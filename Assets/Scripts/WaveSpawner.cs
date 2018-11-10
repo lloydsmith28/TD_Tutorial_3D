6a3031ab31b26379c1fab5c0bb00534a893c3a1a
lloydsmith28 <ag.gaming28@gmail.com>
2018-11-09 19:28:00 -0500

    Turrets Added

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    public Text waveCountdownText;
+
+    private float countdown = 2f;
+    private int waveIndex = 0;
+    private int enemyCount = 0;
+
+    void Update() {
+        if (countdown <= 0f) {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave() {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy() {
+        enemyPrefab.name = "Enemy " + enemyCount;
+
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        enemyCount++;
+    }
+
+}
